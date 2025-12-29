b1282bac52d0436681d0b5b3f3313795746e22c3
Jagruthi Dongari <dongarijagruthi@gmail.com>
2025-12-29 21:45:33 +0530

    Initial commit

---
+++ Calculator.py
@@ -0,0 +1,39 @@
+# Calculator using multiple functions
+def add(a, b):
+    # return sum of two numbers
+    return a + b
+def subtract(a, b):
+    #return difference of two numbers
+    return a - b
+def multiply(a, b):
+    #return product of two numbers
+    return a * b
+def divide(a, b):
+    #handle division by zero
+    if b == 0:
+        return "Cannot divide by zero"
+    return a / b
+
+#main program
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+
+choice = int(input("Enter your choice: "))
+
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+if choice == 1:
+    print("Result:", add(num1,num2))
+elif choice == 2:
+    print("Result:", subtract(num1,num2))
+elif choice == 3:
+    print("Result:", multiply(num1,num2))
+elif choice == 4:
+    print("Result:", divide(num1,num2))
+else:
+    print("Invalid choice")
+
+
